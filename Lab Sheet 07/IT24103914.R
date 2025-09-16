a5e00bd5e0c43755d5f8fca9d1eb3e6031d66d9a
IT24103914 <it24103914@my.sliit.lk>
2025-09-16 22:47:07 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24103914.R
@@ -0,0 +1,15 @@
+setwd("D:\\Users\\nethmi\\OneDrive\\Desktop\\IT24103914")
+
+#Exercise
+# (1)
+punif(25, min=0, max=40, lower.tail=TRUE) - punif(10, min=0, max=40, lower.tail=TRUE)
+
+# (2)
+pexp(2, rate=1/3, lower.tail=TRUE)
+
+# (3)
+# part 1
+pnorm(130, mean=100, sd=15, lower.tail=FALSE)
+
+#part 2
+qnorm(0.95, mean=100, sd=15, lower.tail=TRUE)
